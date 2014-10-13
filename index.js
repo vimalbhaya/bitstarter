1eab47fecdbfe04fba5b81849ad558c9d86aa78d
Vimal Bhaya <vimalbhaya@gmail.com>
2014-10-13 16:24:11 -0700

    Changed Index.html to link to index.js

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+app.get('/', function(request, response) {	
+	var fs  = require('fs') ;
+	var stringBuffer = fs.readFileSync("index.html") ;
+  response.send( stringBuffer.toString()) ;
 })
 
 app.listen(app.get('port'), function() {
